ebc28e50e4b598d3a658481a789099e3d4a35321
Pierre-Eloi Bernard <Pierre-Eloi.Bernard@PVAL.com>
2019-07-08 16:57:25 +0200

    Changing plot interval and adding labels

--- script.py
+++ script.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linsace(0,2*np.pi, 1000)
+x = np.linsace(0,4*np.pi, 1000)
 y = np.sin(x)
 
+plt.xlabel("$x$")
+plt.ylabel("$sin(x)$")
 plt.plot(x,y)
 plt.show()
